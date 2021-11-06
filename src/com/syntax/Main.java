5348b9e1738b4f0c324a257d177ce69c46ac5f36
jmuminov <jmuminov92@gmail.com>
2021-11-06 10:56:33 -0400

    Solar Sytem Basic Structure is Ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem andromeda = new SolarSystem();
+        andromeda.Sun = 1;
+        andromeda.planet = 2;
+        andromeda.moon = 4;
+    }
+}
